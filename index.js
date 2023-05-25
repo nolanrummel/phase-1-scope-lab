12cb04e9a4d563c5a3644dd874881f6fb2bf8aef
Nolan Rummel <nolan.rummel22@gmail.com>
2023-05-25 15:51:29 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+var name = 'not bob'
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'nolan'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not nolan'
+}
